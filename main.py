73eeaaed09d5edab1b54f6182704338e64f9c43a
Heitor-Dairel <hdgtlindo@gmail.com>
2026-03-30 18:40:27 -0300

    ...

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from src.core import MastercardISO8583Parse
-
-
-# import timeit
-
+from rich import print
 
 file = MastercardISO8583Parse()
 
@@ -10,19 +7,4 @@ raw = file.file_contents(date_file="26/05/2025", cycle="CIC2")
 
 iso, iso2 = file.parse_ipm(raw=raw)
 
-from rich import print
-
 print(iso[1])
-
-
-# def fun_to_teste():
-
-#     iso, iso2 = file.parse_ipm(raw=raw)
-
-
-# time_exec = 6
-# result = timeit.timeit(fun_to_teste, number=time_exec)
-
-
-# print(f"Tempo para {time_exec} execuções: {result:.4f} s")
-# print(f"Tempo médio: {result / time_exec:.6f} s")
