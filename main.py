fe54875d1154d6d319743bfa0de27623ec2b8aa7
Heitor-Dairel <hdgtlindo@gmail.com>
2026-08-06 10:46:15 -0300

    adiciona parâmetro de logging nas chamadas do método iso_db para melhor controle de registro

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from src.helpers import list_date
 
 with DB8583() as db:
     for i in list_date():
-        db.iso_db(file_date=i, file_cycle="CIC1")
-        db.iso_db(file_date=i, file_cycle="CIC2")
-        db.iso_db(file_date=i, file_cycle="CIC3")
+        db.iso_db(file_date=i, file_cycle="CIC1", logging=False)
+        db.iso_db(file_date=i, file_cycle="CIC2", logging=False)
+        db.iso_db(file_date=i, file_cycle="CIC3", logging=False)
